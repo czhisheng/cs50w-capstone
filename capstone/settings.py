30be691a5289557b5722fc64154f89d944cb1fdd
Chua Zhisheng <chuazhisheng@hotmail.com>
2025-07-04 00:54:53 +0800

    Updated settings and requirement for database

--- capstone/settings.py
+++ capstone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 from dotenv import load_dotenv
 
@@ -79,10 +80,9 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 AUTH_USER_MODEL = "mycareerpath.User"
